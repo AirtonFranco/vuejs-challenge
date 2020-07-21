61d0553146fb8f633d631b459d46224bae3526ae
AirtonSilveira <a.silveira@accesscredito.com.br>
2020-07-21 17:28:25 -0400

    Configuracao inicial card

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1 class="text-h1">VIVO CONTROLE DIGITAL 2GB ILIM</h1>
+      <h2 class="text-h2">R$ 39,99</h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style>
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
 }
+
+.text-h1 {
+    color: #52D1B2;    
+}
+
+.text-h2 {
+    color: #7A7A7A;
+} 
+
 </style>
---
+++ vuejs/src/main/VueChallenger.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="vuechallenger">
+      <CardOne />
+  </div>
+</template>
+
+<script>
+import CardOne from "..components/CardOne"
+export default {
+    components: {
+        CardOne
+    }
+}
+</script>
+
+<style>
+
+</style>
